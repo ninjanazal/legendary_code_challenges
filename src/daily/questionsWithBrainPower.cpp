886b828be2348c500320acce880c70467bb42b03
Eurico Martins <euricomartinss@gmail.com>
2023-05-14 17:39:58 +0100

    Questiions with brain power init

---
+++ src/daily/questionsWithBrainPower.cpp
@@ -0,0 +1,28 @@
+#include "questionsWithBrainPower.h"
+
+#include <unordered_map>
+#include <vector>
+
+#include "profileBlock.h"
+
+using namespace LendaryCodeChallenges;
+using namespace std;
+
+void QuestionsWithBrainPower::execute() {
+    printf("--- Uncrossed Lines ---\n");
+    vector<vector<int>> input = {{3, 2}, {4, 3}, {4, 4}, {2, 5}};
+}
+
+long long QuestionsWithBrainPower::mostPoints(vector<vector<int>>& questions) {
+    long long output= 0;
+
+    for (int i = 0; i < questions.size(); i++){
+        
+    }
+
+    return output;
+}
+
+void QuestionsWithBrainPower::printResult(long long result) {
+    std::cout << result << std::endl;
+}
---
+++ src/daily/questionsWithBrainPower.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+namespace LendaryCodeChallenges {
+namespace QuestionsWithBrainPower {
+void execute();
+long long mostPoints(vector<vector<int>>& questions);
+void printResult(long long result);
+}  // namespace SpiralMatrix
+}  // namespace LendaryCodeChallenges
