ac431d2aa423c40e26cb6b4b6480bf0b5c29c349
Eurico Martins <euricomartinss@gmail.com>
2024-02-13 00:41:51 +0000

    [1] Two Sum

--- java/app/src/main/java/org/legendary_code_challenges/exercises/ArrayAndHashing.java
+++ java/app/src/main/java/org/legendary_code_challenges/exercises/ArrayAndHashing.java
@@ -45,7 +45,27 @@ public class ArrayAndHashing {
         return false;
       }
     }
-
     return true;
+  }
+
+  /*
+   * [1. Two Sum]
+   * Given an array of integers nums and an integer target, return indices of the
+   * two numbers such that they add up to target.
+   * You may assume that each input would have exactly one solution, and you may
+   * not use the same element twice.
+   * You can return the answer in any order.
+   */
+
+  public static int[] twoSum(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+      int remain = target - nums[i];
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[j] == remain) {
+          return new int[] { i, j };
+        }
+      }
+    }
+    return new int[0];
   }
 }
--- java/app/src/test/java/org/legendary_code_challenges/exercises/ArrayAndHashingTest.java
+++ java/app/src/test/java/org/legendary_code_challenges/exercises/ArrayAndHashingTest.java
@@ -4,34 +4,56 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.*;
+
 public class ArrayAndHashingTest {
-  @Test
-  void containsDuplicateTest() {
-    assertTrue(ArrayAndHashing.containsDuplicate(new int[] { 1, 2, 3, 1 }), "Failded on Example 1:\n" +
-        "\n" +
-        "Input: nums = [1,2,3,1]\n" +
-        "Output: true");
+    @Test
+    void containsDuplicateTest() {
+        assertTrue(ArrayAndHashing.containsDuplicate(new int[] { 1, 2, 3, 1 }), "Failded on Example 1:\n" +
+                "\n" +
+                "Input: nums = [1,2,3,1]\n" +
+                "Output: true");
 
-    assertFalse(ArrayAndHashing.containsDuplicate(new int[] { 1, 2, 3, 4 }), "Failed on Example 2:\n" +
-        "Input: nums = [1,2,3,4]\n" + //
-        "Output: false");
+        assertFalse(ArrayAndHashing.containsDuplicate(new int[] { 1, 2, 3, 4 }), "Failed on Example 2:\n" +
+                "Input: nums = [1,2,3,4]\n" + //
+                "Output: false");
 
-    assertTrue(ArrayAndHashing.containsDuplicate(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }),
-        "Failded on Example 3:\n" +
-            "\n" +
-            "Input: nums = [1,1,1,3,3,4,3,2,4,2]\n" +
-            "Output: true");
-  }
+        assertTrue(ArrayAndHashing.containsDuplicate(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }),
+                "Failded on Example 3:\n" +
+                        "\n" +
+                        "Input: nums = [1,1,1,3,3,4,3,2,4,2]\n" +
+                        "Output: true");
+    }
 
-  @Test
-  void isAnagramTest() {
-    assertTrue(ArrayAndHashing.isAnagram("anagram", "nagaram"), "Failed on Example 1:\n" +
-        "Input: s = \"anagram\", t = \"nagaram\"\n" +
-        "Output: true");
+    @Test
+    void isAnagramTest() {
+        assertTrue(ArrayAndHashing.isAnagram("anagram", "nagaram"), "Failed on Example 1:\n" +
+                "Input: s = \"anagram\", t = \"nagaram\"\n" +
+                "Output: true");
 
-    assertFalse(ArrayAndHashing.isAnagram("rat", "car"), "Failed on Example 2:\n" +
-        "Input: s = \"rat\", t = \"car\"\n" +
-        "Output: false"
-        );
-  }
+        assertFalse(ArrayAndHashing.isAnagram("rat", "car"), "Failed on Example 2:\n" +
+                "Input: s = \"rat\", t = \"car\"\n" +
+                "Output: false");
+    }
+
+    @Test
+    void twoSumTest() {
+        assertTrue(Arrays.equals(new int[] { 0, 1 }, ArrayAndHashing.twoSum(new int[] { 2, 7, 11, 15 }, 9)),
+                "Failed on Exemple 1:\n" +
+                        "Output: [0,1] \n" +
+                        "Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].");
+
+        assertTrue(Arrays.equals(new int[] { 1, 2 }, ArrayAndHashing.twoSum(new int[] { 3, 2, 4 }, 6)),
+                "Failed on Exemple 2:\n" +
+                        "Input: nums = [3,2,4], target = 6\n" +
+                        "Output: [1,2]");
+
+        assertTrue(Arrays.equals(new int[] { 0, 1 }, ArrayAndHashing.twoSum(new int[] { 3, 3 }, 6)),
+                "Failed on Exemple 3:\n" +
+                        "Input: nums = [3,3], target = 6\n" +
+                        "Output: [0,1]");
+
+        assertTrue(Arrays.equals(new int[] {2, 4}, ArrayAndHashing.twoSum(new int[] {-1,-2,-3,-4,-5}, -8)),
+        "Failed on Submission test");
+    }
 }
