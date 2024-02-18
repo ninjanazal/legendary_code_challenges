95350f82b84d88fdc52205d5ccfaf8f7e1a552c2
Eurico Martins <euricomartinss@gmail.com>
2024-02-18 23:07:39 +0000

    [155] Min Stack

--- java/app/src/main/java/org/legendary_code_challenges/exercises/StackEx.java
+++ java/app/src/main/java/org/legendary_code_challenges/exercises/StackEx.java
@@ -43,4 +43,50 @@ public class StackEx {
 		}
 		return stack.empty();
 	}
+
+	/*
+	 * [155. Min Stack]
+	 * Design a stack that supports push, pop, top, and retrieving the minimum
+	 * element in constant time.
+	 * 
+	 * Implement the MinStack class:
+	 * - MinStack() initializes the stack object.
+	 * - void push(int val) pushes the element val onto the stack.
+	 * - void pop() removes the element on the top of the stack.
+	 * - int top() gets the top element of the stack.
+	 * - int getMin() retrieves the minimum element in the stack.
+	 * - You must implement a solution with O(1) time complexity for each function.
+	 * 
+	 * 
+	 */
+	public class MinStack {
+		private ArrayList<int[]> stkPair;
+
+		public MinStack() {
+			stkPair = new ArrayList<int[]>();
+		}
+
+		public void push(int val) {
+			int[] data = new int[]{val, val};
+			
+			if(!stkPair.isEmpty()){
+				int[] prev = stkPair.getLast();
+				data[1] = val <  prev[1] ? val : prev[1];
+			}
+			stkPair.add(data);
+		}
+
+		public void pop() {
+			stkPair.removeLast();
+		}
+
+		public int top() {
+			return stkPair.getLast()[0];
+		}
+
+		public int getMin() {
+			return stkPair.getLast()[1];
+		}
+
+	}
 }
--- java/app/src/test/java/org/legendary_code_challenges/exercises/StackExTest.java
+++ java/app/src/test/java/org/legendary_code_challenges/exercises/StackExTest.java
@@ -15,4 +15,17 @@ public class StackExTest {
     assertFalse(StackEx.isValid("["), "Failed on Submission 2");
 
   }
+
+  @Test
+  void MinStackTest(){
+    StackEx.MinStack minStack = (new StackEx()).new MinStack();
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+
+    assertEquals(-3, minStack.getMin(), "Failed on Example getMin");
+    minStack.pop();
+    assertEquals(0, minStack.top(), "Failed on Example top");
+    assertEquals(-2, minStack.getMin(), "Failed on Example getMin 2");
+  }
 }
