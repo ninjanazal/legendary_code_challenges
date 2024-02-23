13823fca452b93eacddfe4440ab56b5b79e15062
Eurico Martins <euricomartinss@gmail.com>
2024-02-23 00:14:02 +0000

    [853] Car Fleet

--- java/app/src/main/java/org/legendary_code_challenges/exercises/StackEx.java
+++ java/app/src/main/java/org/legendary_code_challenges/exercises/StackEx.java
@@ -194,4 +194,57 @@ public class StackEx {
 
 		return answer;
 	}
+
+	/*
+	 * [853. Car Fleet]
+	 * There are n cars going to the same destination along a one-lane road. The
+	 * destination is target miles away.
+	 * 
+	 * You are given two integer array position and speed, both of length n, where
+	 * position[i] is the position of the ith car and speed[i] is the speed of the
+	 * ith car (in miles per hour).
+	 * 
+	 * A car can never pass another car ahead of it, but it can catch up to it and
+	 * drive bumper to bumper at the same speed. The faster car will slow down to
+	 * match the slower car's speed. The distance between these two cars is ignored
+	 * (i.e., they are assumed to have the same position).
+	 * 
+	 * A car fleet is some non-empty set of cars driving at the same position and
+	 * same speed. Note that a single car is also a car fleet.
+	 * 
+	 * If a car catches up to a car fleet right at the destination point, it will
+	 * still be considered as one car fleet.
+	 * 
+	 * Return the number of car fleets that will arrive at the destination.
+	 */
+	public static int carFleet(int target, int[] position, int[] speed) {
+		class Pair<S, T> {
+			public final S x;
+			public final T y;
+
+			public Pair(S x, T y) {
+				this.x = x;
+				this.y = y;
+			}
+		}
+
+		List<Pair<Integer, Float>> answer = new ArrayList<Pair<Integer, Float>>();
+
+		for (int i = 0; i < position.length; i++) {
+			answer.add(new Pair<Integer, Float>(position[i], (float)(target - position[i]) / (float)speed[i]));
+		}
+
+		Collections.sort(answer, (a, b) -> a.x - b.x);
+
+		int fleets = 1;
+		float last_fleet_time = answer.get(answer.size() - 1).y;
+
+		for (int i = position.length - 2; i >= 0; i--) {
+			if (answer.get(i).y > last_fleet_time) {
+				last_fleet_time = answer.get(i).y;
+				fleets++;
+			}
+		}
+		return fleets;
+	}
 }
