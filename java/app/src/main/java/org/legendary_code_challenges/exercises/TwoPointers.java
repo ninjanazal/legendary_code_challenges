13edc8412cd28503ad520b295dd04ea7681b81c5
Eurico Martins <euricomartinss@gmail.com>
2024-02-25 12:29:17 +0000

    [167] Two Sum II - Input Array Is Sorted

--- java/app/src/main/java/org/legendary_code_challenges/exercises/TwoPointers.java
+++ java/app/src/main/java/org/legendary_code_challenges/exercises/TwoPointers.java
@@ -29,4 +29,37 @@ public class TwoPointers {
     }
     return true;
   }
+
+  /*
+   * [167. Two Sum II - Input Array Is Sorted]
+   * Given a 1-indexed array of integers numbers that is already sorted in
+   * non-decreasing order, find two numbers such that they add up to a specific
+   * target number. Let these two numbers be numbers[index1] and numbers[index2]
+   * where 1 <= index1 < index2 <= numbers.length.
+   * 
+   * Return the indices of the two numbers, index1 and index2, added by one as an
+   * integer array [index1, index2] of length 2.
+   * 
+   * The tests are generated such that there is exactly one solution. You may not
+   * use the same element twice.
+   * 
+   * Your solution must use only constant extra space.
+   */
+  public static int[] twoSum(int[] numbers, int target) {
+    int lPoint = 0;
+    int rPoint = numbers.length - 1;
+    while (lPoint < rPoint) {
+      int result = numbers[lPoint] + numbers[rPoint];
+
+      if (result == target) {
+        return new int[] { lPoint + 1, rPoint + 1 };
+      } else if (result > target) {
+        rPoint--;
+      } else {
+        lPoint++;
+      }
+    }
+
+    return new int[2];
+  }
 }
--- java/app/src/test/java/org/legendary_code_challenges/exercises/TwoPointersTest.java
+++ java/app/src/test/java/org/legendary_code_challenges/exercises/TwoPointersTest.java
@@ -13,4 +13,12 @@ public class TwoPointersTest {
     assertEquals(false, TwoPointers.isPalindrome("race a car"), "Failed on Example 2");
     assertEquals(true, TwoPointers.isPalindrome(" "), "Failed on Example 3");
   }
+
+  @Test
+  void twoSumTest() {
+    assertTrue(Arrays.equals(new int[] { 1, 2 }, TwoPointers.twoSum(new int[] { 2, 7, 11, 15 }, 9)),
+        "Failed on Example 1");
+    assertTrue(Arrays.equals(new int[] { 1, 3 }, TwoPointers.twoSum(new int[] { 2, 3, 4 }, 6)), "Failed on Example 2");
+    assertTrue(Arrays.equals(new int[] { 1, 2 }, TwoPointers.twoSum(new int[] { -1, 0 }, -1)), "Failed on Example 3");
+  }
 }
